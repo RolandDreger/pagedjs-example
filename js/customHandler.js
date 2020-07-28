efb2c226731b5ea40460b606fc93a0b5c8bcaee4
Roland Dreger <mail@rolanddreger.net>
2020-07-28 11:56:09 +0200

    Add custom handlers

---
+++ js/customHandler.js
@@ -0,0 +1,28 @@
+﻿class customHandler extends Paged.Handler {
+	constructor(chunker, polisher, caller) {
+		super(chunker, polisher, caller);
+	}
+
+	beforeParsed(content) {
+		console.log("beforeParsed");
+		// const links = content.querySelectorAll('a[href^="http"], a[href^="www"]');
+		// links.forEach(link => {
+		// 	console.log(link);
+		// });
+	}
+
+	afterParsed(parsed) {
+		console.log("afterParsed");
+		// const images = parsed.querySelectorAll("img");
+		// images.forEach(image => {
+		// 	console.log(image);
+		// });
+	}
+
+
+	afterPageLayout(pageFragment, page) {
+		console.log("afterPageLayout");
+	}
+}
+
+Paged.registerHandlers(customHandler);
